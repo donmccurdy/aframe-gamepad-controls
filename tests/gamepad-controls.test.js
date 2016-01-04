cf6a771e0f8eefbf58d3040596656c13b85aad76
Don McCurdy <don.r.mccurdy@gmail.com>
2016-01-03 22:24:43 -0500

    Add tests and .jshintrc

---
+++ tests/gamepad-controls.test.js
@@ -0,0 +1,47 @@
+var Aframe = require('aframe-core');
+var component = require('../gamepad-controls');
+var entityFactory = require('./helpers').entityFactory;
+
+Aframe.registerComponent('gamepad-controls', component);
+
+var gamepad,
+	gamepadControls;
+navigator.getGamepads = function () { return [gamepad]; };
+
+describe('Gamepad Controls', function () {
+
+	beforeEach(function () {
+		gamepad = {
+			axes: [0, 0, 0, 0],
+			buttons: [],
+			connected: true,
+			id: 'test-gamepad'
+		};
+	});
+
+	beforeEach(function (done) {
+		this.el = entityFactory();
+		this.el.setAttribute('gamepad-controls', '');
+		this.el.addEventListener('loaded', function () {
+			gamepadControls = this.el.components['gamepad-controls'];
+			done();
+		}.bind(this));
+	});
+
+	describe('Gamepad Controls accessors', function () {
+		it('detects a gamepad', function () {
+			assert.equal(gamepadControls.getGamepad(), gamepad);
+		});
+
+		it('returns gamepad ID', function () {
+			assert.equal(gamepadControls.getID(), 'test-gamepad');
+		});
+
+		it('returns joystick state', function () {
+			var joystick0 = gamepadControls.getJoystick(0);
+			assert.equal(joystick0.x, 0);
+			assert.equal(joystick0.y, 0);
+		});
+
+	});
+});
