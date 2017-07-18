dfcadc95518d4a9b860b0f73970fd0895796ba0c
Don McCurdy <don.r.mccurdy@gmail.com>
2017-07-17 19:42:12 -0700

    0.6.0

--- dist/aframe-gamepad-controls.js
+++ dist/aframe-gamepad-controls.js
@@ -176,7 +176,7 @@
 	    velocity[rollAxis] -= velocity[rollAxis] * easing * dt / 1000;
 	    velocity[pitchAxis] -= velocity[pitchAxis] * easing * dt / 1000;
 
-	    var position = el.getComputedAttribute('position');
+	    var position = el.getAttribute('position');
 
 	    if (data.enabled && data.movementEnabled && gamepad) {
 	      var dpad = this.getDpad(),
@@ -212,7 +212,7 @@
 	        rotation = new THREE.Vector3();
 
 	    this._getMovementVector = function (dt) {
-	      rotation.copy(this.el.getComputedAttribute('rotation'));
+	      rotation.copy(this.el.getAttribute('rotation'));
 	      this.direction.copy(this.velocity);
 	      this.direction.multiplyScalar(dt / 1000);
 	      if (!rotation) { return this.direction; }
